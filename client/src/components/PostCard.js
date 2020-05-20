12e85eadb11718679c5ddbb9f7de8817a4f765c2
tizah <davidzagi93@gmail.com>
2020-05-20 16:24:20 +0100

    post card

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Card, Icon, Label, Image, Button } from "semantic-ui-react";
+import { Card, Icon, Label, Image, Button, Popup } from "semantic-ui-react";
 import moment from "moment";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth";
@@ -33,14 +33,20 @@ function PostCard(props) {
       <Card.Content extra>
         <LikeButton user={user} post={{ id, likes }} />
 
-        <Button labelPosition="right" as={Link} to={`/post/${id}`}>
-          <Button color="blue" basic>
-            <Icon name="comments" />
-          </Button>
-          <Label as="a" basic color="blue" pointing="left">
-            {comments.length}
-          </Label>
-        </Button>
+        <Popup
+          content="Comment on post"
+          inverted
+          trigger={
+            <Button labelPosition="right" as={Link} to={`/post/${id}`}>
+              <Button color="blue" basic>
+                <Icon name="comments" />
+              </Button>
+              <Label as="a" basic color="blue" pointing="left">
+                {comments.length}
+              </Label>
+            </Button>
+          }
+        />
         {user && user.username === username && (
           <Button
             as="div"
